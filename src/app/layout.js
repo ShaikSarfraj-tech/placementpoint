f85e71619233c4b64c0eb87f7da6a2243f5b855b
Sarfraj Shaik <58382195+ShaikSarfraj-tech@users.noreply.github.com>
2025-04-05 09:19:31 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -71,6 +71,24 @@ export default function RootLayout({ children }) {
   } else {
     document.documentElement.classList.remove('dark')
   }`} </Script>
+        <Script id="ad-script" strategy="afterInteractive">
+  {`
+    atOptions = {
+      'key' : 'fc654f82e592a48e6dff6999e3a7f50d',
+      'format' : 'iframe',
+      'height' : 60,
+      'width' : 468,
+      'params' : {}
+    };
+  `}
+</Script>
+
+<Script
+  id="ad-script-src"
+  strategy="afterInteractive"
+  src="//www.highperformanceformat.com/fc654f82e592a48e6dff6999e3a7f50d/invoke.js"
+/>
+
 <script type="text/javascript" src="//www.highperformanceformat.com/fc654f82e592a48e6dff6999e3a7f50d/invoke.js"></script>
         <Header />
         {children}
